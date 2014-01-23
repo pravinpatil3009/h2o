c1a3a524a7005443f6f999a5f9f4caf665fce8f5
Tim Case <tim@2drops.net>
2014-01-22 18:51:03 -0800

    add support for first time user logs in from canvas

--- app/controllers/canvas_auth_controller.rb
+++ app/controllers/canvas_auth_controller.rb
@@ -1,13 +1,41 @@
 class CanvasAuthController < ApplicationController
 
   def authorize
-    canvas_auth = CanvasAuth.new(request)
+    #canvas_auth = CanvasAuth.new(request)
 
-    if canvas_auth.valid?
-      render :text => "AUTHORIZED"
-    else
-      render :text => "DENIED"
-    end
+    #if canvas_auth.valid?
+    #lookup_user
+    #if user
+    #login_user
+    #redirect_to_dashboard
+    #else
+    set_flash_message
+    set_session_with_canvas_user_id
+    redirect_to_h2o_login
+    #end
+    #flash[:notice] = 'ladi dadi'
+    #session[:canvas_user_id] = '02c18b8291c5f3b1ac400184c3882698bb66ca9e'#params.fetch(:user_id)
+    #redirect_to new_user_session_path
+      #render :text => "AUTHORIZED"
+    #else
+      #render 'unauthorized'
+    #end
+  end
+
+  def set_session_with_canvas_user_id
+    session[:canvas_user_id] = '02c18b8291c5f3b1ac400184c3882698bb66ca9e'#params.fetch(:user_id)
+  end
+
+  def redirect_to_h2o_login
+    redirect_to new_user_session_path
+  end
+
+  def set_flash_message
+    flash[:notice] =
+    "You are logging in to H2o directly from Canvas for the first time.<br/><br/>
+     After you login your Canvas id will be attached to your H2o id
+     and the next time you initiate an H2o session from Canvas you'll be logged in
+     automatically.".html_safe
   end
 
   def lti_config
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,17 +2,18 @@ class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
   protect_from_forgery :except => [:create]
-  
+
   def new
     @user_session = UserSession.new
     render :layout => (request.xhr?) ? false : true
   end
-  
+
   def create
     @user_session = UserSession.new(params[:user_session])
     @user_session.save do |result|
       if result
         apply_user_preferences(@user_session.user)
+        save_canvas_id_to_current_user if first_time_canvas_login?
         if request.xhr?
           #Text doesn't matter, status code does.
           render :text => 'Success!', :layout => false
@@ -24,10 +25,25 @@ class UserSessionsController < ApplicationController
       end
     end
   end
-  
+
   def destroy
     destroy_user_preferences(current_user)
     current_user_session.destroy
     redirect_back_or_default "/"
   end
+
+  def first_time_canvas_login?
+    session.key?(:canvas_user_id)
+  end
+
+  def save_canvas_id_to_current_user
+    @user_session.user.update_attribute(:canvas_id, session.fetch(:canvas_user_id))
+    clear_canvas_id_from_session
+  end
+
+  def clear_canvas_id_from_session
+    session[:canvas_user_id] = nil
+  end
+
+
 end
